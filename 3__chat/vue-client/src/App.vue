a0ca38acbd6f2d65392dd5c0492bcff1740a41db
JonJaryongLee <director9303@gmail.com>
2025-09-09 15:22:17 +0900

    updated

--- 3__chat/vue-client/src/App.vue
+++ 3__chat/vue-client/src/App.vue
@@ -3,17 +3,7 @@ import { ref } from "vue";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
-const nickname = ref("");
-const isNicknameInputDisabled = ref(false);
-const inputMessage = ref("");
-
-const nextId = ref(1);
-// {
-//   id: 1,
-//   nickname: "jony",
-//   text: "안녕?",
-// }
-const messages = ref([]);
+const message = ref("");
 
 const socket = new WebSocket(SOCKET_URL);
 
@@ -24,9 +14,7 @@ socket.onopen = () => {
 // 서버에서 메시지를 받으면 출력
 socket.onmessage = async (event) => {
   const text = await event.data.text();
-  const message = JSON.parse(text);
-  message.id = ++nextId.value;
-  messages.value.push(message);
+  console.log(text);
 };
 
 socket.onclose = () => {
@@ -38,73 +26,15 @@ socket.onerror = (event) => {
 };
 
 function sendMessage() {
-  if (nickname.value === "" || nickname.value.trim() === "") {
-    return;
-  }
-  if (inputMessage.value === "" || inputMessage.value.trim() === "") {
-    inputMessage.value = "";
-    return;
-  }
-  const message = {
-    nickname: nickname.value,
-    text: inputMessage.value,
-  };
   try {
-    socket.send(JSON.stringify(message));
+    socket.send(message.value);
   } catch (error) {
     console.error(error);
   }
-  message.id = ++nextId.value;
-  messages.value.push(message);
-  inputMessage.value = "";
-}
-
-function setNicknameInputDisabled() {
-  if (nickname.value === "" || nickname.value.trim() === "") {
-    alert("닉네임을 입력하세요");
-    nickname.value = "";
-    return;
-  }
-  nickname.value = nickname.value.trim();
-  isNicknameInputDisabled.value = true;
 }
 </script>
 
 <template>
-  <div class="nickname-wrapper">
-    <input
-      type="text"
-      placeholder="닉네임을 입력하세요"
-      v-model="nickname"
-      :disabled="isNicknameInputDisabled"
-      @keyup.enter="setNicknameInputDisabled"
-    />
-    <button @click="setNicknameInputDisabled">확정</button>
-  </div>
-
-  <div class="message-wrapper">
-    <input
-      type="text"
-      placeholder="메세지를 입력하세요"
-      v-model="inputMessage"
-      @keyup.enter="sendMessage"
-    />
-    <button @click="sendMessage">보내기</button>
-  </div>
-
-  <ul class="message-ul">
-    <li v-for="message in messages" :key="message.id">
-      <span class="nickname">[{{ message.nickname }}]</span>
-      <span class="message">{{ message.text }}</span>
-    </li>
-  </ul>
+  <input type="text" v-model="message" />
+  <button @click="sendMessage">보내기</button>
 </template>
-
-<style scoped>
-.message-ul {
-  padding: 0;
-}
-.message-ul > li {
-  list-style: none;
-}
-</style>
